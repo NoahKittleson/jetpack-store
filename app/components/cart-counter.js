ee6472c7a8243ba2177c584f49b66adb4076521b
Daniel Wulff and Noah Kittleson <noah@>
2016-06-15 16:20:56 -0700

    calculated sale prices correctly

--- app/components/cart-counter.js
+++ app/components/cart-counter.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
-
+  
   cartCount: Ember.computed('shoppingCart.items.[]', function() {
     return this.get('shoppingCart').items.length;
   })
--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  shoppingCart: Ember.inject.service()
+
+  actualPrice: Ember.computed('item.price', 'item.sale', function() {
+    return (this.get('item.price') -  this.get('item.price') * (this.get('item.sale') / 100.00)).toFixed(2);
+  })
 });
